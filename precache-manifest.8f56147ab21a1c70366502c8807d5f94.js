7c0b878b5cc8957d3e04ebcf6aea9b305c2faea0
Santiago <gustavo.santiago@vistajet.com>
2019-02-27 15:24:14 -0300

    Updates

---
+++ precache-manifest.8f56147ab21a1c70366502c8807d5f94.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e89512b74f599afab7b1",
+    "url": "/static/css/main.da9a0d70.chunk.css"
+  },
+  {
+    "revision": "e89512b74f599afab7b1",
+    "url": "/static/js/main.e89512b7.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c80c97b4f303635f2b95",
+    "url": "/static/js/2.c80c97b4.chunk.js"
+  },
+  {
+    "revision": "da3d45ac8084c372f1c5b200b8bb7fff",
+    "url": "/static/media/CoolveticaRg-Regular.da3d45ac.woff"
+  },
+  {
+    "revision": "20bea4601c00e02b9ca019159ac5a8a9",
+    "url": "/static/media/CoolveticaRg-Regular.20bea460.woff2"
+  },
+  {
+    "revision": "756d9b385c7cd8c604a48b3304915847",
+    "url": "/static/media/I-pixel-u.756d9b38.woff"
+  },
+  {
+    "revision": "b13969befa6a464bee888310115225d1",
+    "url": "/static/media/I-pixel-u.b13969be.woff2"
+  },
+  {
+    "revision": "2fba1d2deef300b37c983c3bb3a2571f",
+    "url": "/index.html"
+  }
+];
