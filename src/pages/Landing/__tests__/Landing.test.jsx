e173c980770eeacbf335378ff0e5b6c8ae1fa7f6
Santiago <gustavo.santiago@vistajet.com>
2019-02-27 14:51:16 -0300

    :wrench: Fix prepush test hook, add complete test battery

--- src/pages/Landing/__tests__/Landing.test.jsx
+++ src/pages/Landing/__tests__/Landing.test.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '..';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { Header } from '../styles';
+
+describe('Testing Landing.styles...', () => {
+  configure({ adapter: new Adapter() });
+
+  it('Header should render correctly without props', () => {
+    const wrapper = shallow(<Header />);
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
 });
