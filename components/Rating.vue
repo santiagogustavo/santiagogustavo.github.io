650837550d0246b289c132246a2263eec166ff0f
santiagogustavo <santiago.gustavo.mail@gmail.com>
2022-07-23 04:00:37 -0300

    feat(resume): add page + base layout, add qrcode, implement Rating

---
+++ components/Rating.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="rating">
+    <v-icon v-for="count in full" :key="`star-full-${count}`">
+      mdi-star
+    </v-icon>
+    <v-icon v-for="count in half" :key="`star-half-${count}`">
+      mdi-star-half-full
+    </v-icon>
+    <v-icon v-for="count in outline" :key="`star-outline-${count}`">
+      mdi-star-outline
+    </v-icon>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Rating',
+  props: {
+    value: {
+      type: Number,
+      required: true,
+    },
+  },
+  computed: {
+    full() {
+      return Math.floor(this.value);
+    },
+    half() {
+      return Math.ceil(this.value % 1);
+    },
+    outline() {
+      return 5 - this.full - this.half;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.rating {
+  display: flex;
+  align-items: center;
+  i {
+    flex: 0;
+  }
+}
+</style>
---
+++ pages/resume.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="resume">
+    <h1>Gustavo Santiago</h1>
+    <img class="resume__qrcode" :src="QrCode" alt="QR Code" />
+    <br />
+    <p>
+      <v-icon>mdi-map-marker</v-icon>
+      Piracicaba, São Paulo, Brazil
+    </p>
+    <p>
+      <v-icon>mdi-chat</v-icon>
+      +55 19 995179798
+    </p>
+    <p>
+      <v-icon>mdi-email</v-icon>
+      <a href="mailto:santiago.gustavo.mail@gmail.com">
+        santiago.gustavo.mail@gmail.com
+      </a>
+    </p>
+    <p>
+      <v-icon>mdi-linkedin</v-icon>
+      <a href="https://www.linkedin.com/in/santiago-gustavo/" target="_blank">
+        https://www.linkedin.com/in/santiago-gustavo/
+      </a>
+    </p>
+    <p>
+      <v-icon>mdi-cursor-default-click</v-icon>
+      <a href="https://santiagogustavo.github.io/" target="_blank">
+        https://santiagogustavo.github.io/
+      </a>
+    </p>
+    <br /><br />
+    <h4>
+      <v-icon>mdi-auto-fix</v-icon>
+      Principais competências
+    </h4>
+    <h4>
+      <v-icon>mdi-translate</v-icon>
+      Idiomas
+    </h4>
+    <h4>
+      <v-icon>mdi-seal</v-icon>
+      Certificações
+    </h4>
+    <br /><br />
+    <h3>
+      <v-icon>mdi-briefcase</v-icon>
+      Experiência
+    </h3>
+    <h3>
+      <v-icon>mdi-school</v-icon>
+      Formação acadêmica
+    </h3>
+    <br />
+    <Rating :value="0" />
+    <Rating :value="2" />
+    <Rating :value="3.5" />
+    <Rating :value="5" />
+  </div>
+</template>
+
+<script>
+import QrCode from '@/assets/imgs/qrcode.png';
+import Rating from '@/components/Rating.vue';
+
+export default {
+  name: 'Resume',
+  components: {
+    Rating,
+  },
+  data() {
+    return {
+      QrCode,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.resume {
+  max-height: 100vh;
+  overflow: auto;
+
+  &__qrcode {
+    width: 150px;
+  }
+}
+</style>
