d1012aed6a81a50d2e786d05fdebe5bf4fda1711
santiagogustavo <santiago.gustavo.mail@gmail.com>
2024-03-20 16:31:33 -0300

    a91e36f

---
+++ _nuxt/a7c1a04.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{387:function(e,n,t){"use strict";t.r(n);var c={name:"Watcher",mounted:function(){this.$emit("created")}},r=t(12),component=Object(r.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("span")}),[],!1,null,null,null);n.default=component.exports}}]);
